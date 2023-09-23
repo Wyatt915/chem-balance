6c515452c68172dffc608994b08c4a51b9645c75
Wyatt Sheffield <cws@wyatts.xyz>
2023-09-23 00:57:12 -0500

    Just make sure the RHS is all negative

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ void chem_balance(matrix* m){
     }
     frac scalar = itofrac(lcm_list(freecolumn, count));
     for (size_t r = 0; r < count; r++){
-        coefficients[r] = fr_mul(scalar, m->data[r][m->c-1]).n;
+        coefficients[r] = -1*fr_mul(scalar, m->data[r][m->c-1]).n;
     }
     coefficients[count] = scalar.n * divisor;
     for (size_t i = 0; i < m->c; i++){
@@ -30,14 +30,20 @@ void chem_balance(matrix* m){
 }
 
 int main(){
-    int mydata[30] = {
-        1, 0 ,  0, 0 , 0 , 2,
-        1, 0 , 2, 0 , 0 , 0,
-        3, 11,  0, 2, 1, 3,
-        0, 12,  0, 1, 0 , 1,
-        0, 22,  0, 0, 2, 0
+    // int mydata[30] = {
+    //     1,  0,   0,   0,   0,   -2,
+    //     1,  0,   -2,  0,   0,   0,
+    //     3,  11,  0,   -2,  -1,  -3,
+    //     0,  12,  0,   -1,  0,   -1,
+    //     0,  22,  0,   0,   -2,  0
+    // };
+    // matrix* mymatrix = createMatrix(5,6);
+    int mydata[12] = {
+        6,0,-1,0,
+        14,0,0,-2,
+        0,2,-2,-1
     };
-    matrix* mymatrix = createMatrix(5,6);
+    matrix* mymatrix = createMatrix(3,4);
     loadIntValues(mymatrix, mydata);
     printMatrix(mymatrix);
     rref(mymatrix);
